888b64e8222e8ae659b92470b3ed5d1720d3a1f5
yibin <yibin.net@qq.com>
2013-02-17 19:25:55 +0800

    merge

---
+++ WeatherReport/WeatherReport/YBTestViewController.m
@@ -0,0 +1,31 @@
+//
+//  YBTestViewController.m
+//  WeatherReport
+//
+//  Created by yibin on 13-2-16.
+//  Copyright (c) 2013年 us.yibin. All rights reserved.
+//
+
+#import "YBTestViewController.h"
+
+@implementation YBTestViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
