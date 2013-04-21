a9ac38a43782f2fec5331928d7927d4c7b8eceb5
yibin <yibin.net@qq.com>
2013-04-21 08:37:46 +0800

    增加对WorldWeatherOnline API的支持

---
+++ WeatherReport/FreeWeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  FreeWeatherViewController.h
+//  WeatherReport
+//
+//  Created by yibin on 13-4-21.
+//  Copyright (c) 2013年 us.yibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FreeWeatherViewController : UIViewController
+
+@end
---
+++ WeatherReport/FreeWeatherViewController.m
@@ -0,0 +1,38 @@
+//
+//  FreeWeatherViewController.m
+//  WeatherReport
+//
+//  Created by yibin on 13-4-21.
+//  Copyright (c) 2013年 us.yibin. All rights reserved.
+//
+
+#import "FreeWeatherViewController.h"
+
+@interface FreeWeatherViewController ()
+
+@end
+
+@implementation FreeWeatherViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
